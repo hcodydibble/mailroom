a1debc79fbd842e3a8a25c35c4b50d7b0023788e
gabrielx52 <gabriel.meringolo@gmail.com>
2017-10-20 14:48:28 -0700

    Added get user input function, with py2/3 version specific input functions.

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,6 +1,27 @@
-"""."""
+"""Python based package to automate charity donor emails and reports."""
+
+DONOR_DATABASE = {}
 
 
 def main():
-    """."""
-    return 'Main func!!'
+    """Main handler function for charity thank you email and report creator."""
+    pass
+
+
+def create_a_report():
+    """"Create sorted report of donors with donation data."""
+    pass
+
+
+def send_a_thank_you():
+    """"Return a thank you letter for a specified donor."""
+    pass
+
+
+def get_user_input():
+    """Get and return user input with py2/3 version handling."""
+    try:
+        user_input = raw_input('py2')
+    finally:
+        user_input = input('py3')
+    return user_input
